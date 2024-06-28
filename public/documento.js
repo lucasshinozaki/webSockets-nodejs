3c300cb7454a5cdcf986bebe2b4c039f131f144a
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2024-06-28 17:31:38 -0300

    Enviando para salas corretas

--- public/documento.js
+++ public/documento.js
@@ -11,7 +11,10 @@ tituloDocumento.textContent = nomeDocumento || "Documento sem titulo"
 selecionarDocumento(nomeDocumento)
 
 textoEditor.addEventListener("keyup", () => {
-    emitirTextoEditor(textoEditor.value)
+    emitirTextoEditor({
+        texto: textoEditor.value, 
+        nomeDocumento
+    })
 })
 
 function atualizaTextoEditor(texto) {
