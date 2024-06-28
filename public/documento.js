66a34e7ce79a5e99fd4e44e12b1931909a35d6c9
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2024-06-28 17:27:35 -0300

    Direcionando o envio da mensagem

--- public/documento.js
+++ public/documento.js
@@ -1,6 +1,14 @@
-import { emitirTextoEditor } from "./socket-front-document.js"
+import { emitirTextoEditor, selecionarDocumento } from "./socket-front-document.js"
+
+const paramentros = new URLSearchParams(window.location.search)
+const nomeDocumento = paramentros.get("nome")
 
 const textoEditor = document.getElementById("editor-texto")
+const tituloDocumento = document.getElementById("titulo-documento")
+
+tituloDocumento.textContent = nomeDocumento || "Documento sem titulo"
+
+selecionarDocumento(nomeDocumento)
 
 textoEditor.addEventListener("keyup", () => {
     emitirTextoEditor(textoEditor.value)
