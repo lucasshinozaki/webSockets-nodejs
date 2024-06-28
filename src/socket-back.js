75d9405b39a4c23f2c15ad62cc27f4d9e33c85a0
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2024-06-28 17:36:39 -0300

    Armazenando dados localmente

--- src/socket-back.js
+++ src/socket-back.js
@@ -1,9 +1,26 @@
 import io from "./servidor.js"
 
+const documentos = [
+    {
+        nome: "JavaScript",
+        texto: "texto de js",
+    },
+    {
+        nome: "Node",
+        texto: "texto de node",
+    },
+    {
+        nome: "Socket.IO",
+        texto: "texto de socket,io",
+    },
+]
+
 io.on("connection", (socket) => {
     console.log('Um cliente se conectou! ID', socket.id)
 
     socket.on("selecionar_documento", (nomeDocumento) => {
+        const documento = encontrarDocumento(nomeDocumento)
+        console.log(documento)
         socket.join(nomeDocumento)
     })
 
@@ -12,3 +29,11 @@ io.on("connection", (socket) => {
         socket.to(nomeDocumento).emit("texto_editor_clientes", texto)
     })
 })
+
+function encontrarDocumento(nome) {
+    const documento = documentos.find((documento) => {
+        return documento.nome === nome
+    })
+
+    return documento
+}
