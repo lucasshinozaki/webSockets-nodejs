ed3815bab48b5da4e33d7277f9a27b79d79340cf
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2024-06-28 17:51:05 -0300

    Devolvendo dados para o front

--- src/socket-back.js
+++ src/socket-back.js
@@ -10,7 +10,7 @@ const documentos = [
         texto: "texto de node",
     },
     {
-        nome: "Socket.IO",
+        nome: "Socket.io",
         texto: "texto de socket,io",
     },
 ]
@@ -18,15 +18,25 @@ const documentos = [
 io.on("connection", (socket) => {
     console.log('Um cliente se conectou! ID', socket.id)
 
-    socket.on("selecionar_documento", (nomeDocumento) => {
-        const documento = encontrarDocumento(nomeDocumento)
-        console.log(documento)
+    socket.on("selecionar_documento", (nomeDocumento, devolverTexto) => {
         socket.join(nomeDocumento)
+        const documento = encontrarDocumento(nomeDocumento)
+        
+        if (documento) {
+            //socket.emit("texto_documento", documento.texto)
+            devolverTexto(documento.texto)
+        }
     })
 
     socket.on("texto_editor", ({texto, nomeDocumento}) => {
+        const documento = encontrarDocumento(nomeDocumento)
+
+        if (documento) {
+            documento.texto = texto
+            socket.to(nomeDocumento).emit("texto_editor_clientes", texto)
+        }
+
         //socket.broadcast.emit("texto_editor_clientes", texto)
-        socket.to(nomeDocumento).emit("texto_editor_clientes", texto)
     })
 })
 
