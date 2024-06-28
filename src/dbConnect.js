34baad971c6977a9f6589f212b23a6fbb117a36b
Lucas Shinozaki <lucas_shinozaki@hotmail.com>
2024-06-28 18:50:48 -0300

    Obtendo dados do banco

--- src/dbConnect.js
+++ src/dbConnect.js
@@ -4,15 +4,15 @@ dotenv.config();
 
 const cliente = new MongoClient(process.env.DB_CONNECT)
 
-let documentoColecao
+let documentosColecao
 
 try {
     await cliente.connect()
     const db = cliente.db("websockets")
-    documentoColecao = db.collection('documentos')
+    documentosColecao = db.collection('documentos')
     console.log("Conectado ao bd")
 } catch(erro) {
     console.log(erro)
 }
 
-export {documentoColecao}
+export {documentosColecao}
